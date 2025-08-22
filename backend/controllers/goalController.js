4129c3ce58df95d3ba360317f5a0879cacf06e01
mozhinag <manimozhinagooran06@gmail.com>
2025-08-22 09:50:03 +0530

    REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,18 @@
 import expressAsyncHandler from "express-async-handler";
+import Goal from "../models/goalModel.js";
+
+
+
+
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 
 export const getGoals = expressAsyncHandler(async(req,res) =>{
     
-res.status(200).json({msg:' Get Goals'})
+    const goals = await Goal.find();
+res.status(200).json(goals);
 }
 )
 
@@ -19,8 +26,10 @@ export const setGoal =expressAsyncHandler( async(req, res) => {
             throw new Error('Please add a text field')
 
     }
+ 
+    const goal = await Goal.create({text:req.body.text})
+    res.status(201).json(goal);
 
-    res.status(200).json({ msg: 'Set Goal', text: req.body.text });
 })
 
 
@@ -28,15 +37,37 @@ export const setGoal =expressAsyncHandler( async(req, res) => {
 // @route   PUT /api/goals/:id
 // @access  Private
 
- export const updateGoal = expressAsyncHandler(async(req,res) =>{
-    res.status(200).json({msg:`Update Goals${req.params.id}`})
-}
+export const updateGoal = expressAsyncHandler(async (req, res) => {
 
-)
+    
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true }   
+  );
+
+res.status(200).json(updatedGoal);
+
+});
+
+
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
 
- export const deleteGoal = expressAsyncHandler(async(req, res) =>{
-    res.status(200).json({msg:`Delete Goals ${req.params.id}`})
-})
+ export const deleteGoal = expressAsyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  await goal.deleteOne();  
+
+  res.status(200).json({ id: req.params.id });
+});
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
-import express from 'express'
+import express from 'express';
+import colors from 'colors';
 import goalRoutes from './routes/goalRoutes.js';
 import  { errorHandler }  from './middleware/errorMiddleware.js';
+
+import {connectDB} from './config/db.js';
+
 const port = process.env.PORT || 5000;
 
-const app = express();
+connectDB();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended:false }))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 
 app.use('/api/goals',goalRoutes)
