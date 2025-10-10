13d980c2f16a90249df40afa6e6ef6e5f14dcff5
mozhinag <manimozhinagooran06@gmail.com>
2025-10-10 12:26:57 +0530

    empty

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,8 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"))
   );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
 }
 
 app.listen(port,() =>{
