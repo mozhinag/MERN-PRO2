0c3e6bbd7cc1bfacf65c76c0ae97f81ab9d55e58
mozhinag <manimozhinagooran06@gmail.com>
2025-12-17 11:21:39 +0530

    production

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,15 @@ import mongoose from "mongoose";
 
  export const connectDB =async()=>{
     try {
-        
+
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB Connected : ${conn.connection.host}`.cyan.underline);
-        
+
     } catch (error) {
 console.log(error);
-process.exit(1);
-        
+    if (process.env.NODE_ENV !== "production") {
+      process.exit(1);
+    }
+
     }
 }
